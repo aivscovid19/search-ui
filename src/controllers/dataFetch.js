715f0d7474c656e7bbe8477398a6f69a19dced55
Colton Ehrman <coltonje95@gmail.com>
2020-06-29 19:31:31 -0500

    update fetch request with new endpoint and use axios for fetching

--- src/controllers/dataFetch.js
+++ src/controllers/dataFetch.js
@@ -1,13 +1,10 @@
+import axios from 'axios';
+
 export const fetchData = async search_term => {
-  const CLOUD_URL = "https://us-central1-for-ivan.cloudfunctions.net/foamtree"
+  const CLOUD_URL = "https://us-central1-for-web-search.cloudfunctions.net/search"
   
-  const response = await fetch(CLOUD_URL, {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ search_term })
-  });
-  
-  return response.json();
+  const { data } = await axios.post(CLOUD_URL, { search_term, group: true });
+  return data;
 };
 
 export const findDocs = ({ groups, _docs = [] }) => {
