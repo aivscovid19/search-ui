be2ad0146ab3400c61e483ac415f6d269d00a36b
Ivan Kozlov <ivaan.kozlov@gmail.com>
2020-07-01 03:11:37 -0700

    fix: add default value for groups

--- src/controllers/dataFetch.js
+++ src/controllers/dataFetch.js
@@ -7,7 +7,7 @@ export const fetchData = async search_term => {
   return data;
 };
 
-export const findDocs = ({ groups, _docs = [] }) => {
+export const findDocs = ({ groups = [], _docs = [] }) => {
   let docs = [ ..._docs ];
   groups.forEach(g => docs = [ ...docs, ...findDocs(g) ]);
   return docs;
