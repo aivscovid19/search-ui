63b1c57ca385a9bdcc5cf4277b61220e98629d2e
dkurgan <dunkanup@gmail.com>
2020-07-20 11:50:04 -0700

    deleted url var

--- src/controllers/dataFetch.js
+++ src/controllers/dataFetch.js
@@ -2,14 +2,12 @@ import axios from 'axios';
 import { CLOUD_URL, CLOUD_DEEP_URL } from '../config';
 
 export const fetchDeepSearch = async ({ query, email, name, type }) => {
-  const url = CLOUD_DEEP_URL;
-  const { data } = await axios.post(url, { query, email, name, type });
+  const { data } = await axios.post(CLOUD_DEEP_URL, { query, email, name, type });
   return data;
 };
 
 export const fetchData = async search_term => {
-  const url = CLOUD_URL;
-  const { data } = await axios.get(url, { search_term, group: true });
+  const { data } = await axios.get(CLOUD_URL, { search_term, group: true });
   return data;
 };
 
