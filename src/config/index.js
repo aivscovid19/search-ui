1be40b2cc028ece8806ce4713cc49416479cb33c
Ivan Kozlov <ivaan.kozlov@gmail.com>
2020-08-10 18:35:50 -0700

    fix: add static ip address of backend instance

--- src/config/index.js
+++ src/config/index.js
@@ -1,2 +1,2 @@
-export const CLOUD_URL = "http://34.70.163.3:5000/api/search";
+export const CLOUD_URL = "http://35.247.69.216:5000/api/search";
 export const CLOUD_DEEP_URL = "https://us-central1-for-web-search.cloudfunctions.net/job-organizer";
