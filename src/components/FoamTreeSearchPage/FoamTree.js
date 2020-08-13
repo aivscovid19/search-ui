069a7b5b285346b73ff059aca352bd6dcfedd4d9
Ivan Kozlov <ivaan.kozlov@gmail.com>
2020-08-13 03:58:24 -0700

    fix: docs is undefined when navigating to a single document

--- src/components/FoamTreeSearchPage/FoamTree.js
+++ src/components/FoamTreeSearchPage/FoamTree.js
@@ -26,7 +26,7 @@ const FoamTree = ({
       if (group && group.unselectable) {
         event.preventDefault();
       } else if (group) {
-        const docs = (group.groups) ? findDocs(group) : group._docs;
+        const docs = findDocs(group);
         setResultCount([docs.length, totalCount]);
         setDocs(docs);
       }
