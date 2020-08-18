b51d8720c8ced307b3411dc00550be41c95210cb
Victor <victor.as.nascimento@gmail.com>
2020-08-17 21:54:49 -0300

    fix if empty string

--- src/components/KeywordsResultsCard/index.js
+++ src/components/KeywordsResultsCard/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './styles.css';
-import { Tooltip } from '@material-ui/core'
+import { Tooltip } from '@material-ui/core';
+import SlideView from '../../components/SlideView';
 
 // The keyword component takes a list as an argument
 // anchor tag was used in case we decide that clicking
 // the keyword would produce a search specific to that word
+
+// 
 
 // Currently I am getting phrases in the keyword container
 // This breaks the UI if the string is too long, so I truncate
@@ -15,21 +18,26 @@ import { Tooltip } from '@material-ui/core'
 // Next step would be to add a slider that allows us to view all keys
 
 export const KeywordsDisplay = ({ keywords }) => {
+    console.log(keywords)
     return (
-        <span className="keywords-container">
-            <span className="keywords">{keywords.slice(0, 5).map((keyword, index) => {
-                return (
-                    <Tooltip title={keyword}>
-                        <a key={keyword} className="individual-keywords">
-                            {
-                                keyword.length > 20
-                                    ? keyword.slice(0, 20).trim() + "..."
-                                    : keyword
-                            }
-                        </a>
-                    </Tooltip>
-                );
-            })}</span>
-        </span>
+        <div className="keywords-container">
+            {keywords && (
+                <div className="keywords">
+                    {keywords.map((keyword, index) => {
+                        return (
+                            <Tooltip title={keyword}>
+                                <a key={index} className="individual-keywords">
+                                    {
+                                        keyword.length > 20
+                                            ? keyword.slice(0, 20).trim() + "..."
+                                            : keyword
+                                    }
+                                </a>
+                            </Tooltip>
+                        );
+                    })}
+                </div>
+            )}
+        </div>
     );
 }
