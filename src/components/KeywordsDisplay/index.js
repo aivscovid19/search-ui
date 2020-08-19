704c452d0025fdfba864f8b4da2b38651afc4622
Victor <victor.as.nascimento@gmail.com>
2020-08-19 20:23:04 -0300

    cleaning unneeded comments, new line at file end

--- src/components/KeywordsDisplay/index.js
+++ src/components/KeywordsDisplay/index.js
@@ -2,13 +2,8 @@ import React from 'react';
 import './styles.css';
 import { Tooltip } from '@material-ui/core';
 import shuffleArray from '../helpers/shuffleArray'
-// import * as icons from '../../icons';
-
-// The keyword component takes a list as an argument
 
 export const KeywordsDisplay = ({ keywords }) => {
-    // currently receiving a single-item list and spliting
-    // keyword area shortened by randomizing and then slicing list
     console.log(keywords)
     const keywordsToMap = shuffleArray(keywords).slice(0,6)
 
@@ -16,8 +11,6 @@ export const KeywordsDisplay = ({ keywords }) => {
         <div className="keywords-container">
             {keywordsToMap && (
                 <div className="keywords">
-                    {/* could add an icon infront */}
-                    {/* <icons.Key width="1rem" height="1rem"/> */}
                     {keywordsToMap.map((keyword, index) => {
                         return (
                             <Tooltip title={keyword}>
