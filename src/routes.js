d566c9000bb0bf56a3448024c72a68808f6f0295
Victor <victor.as.nascimento@gmail.com>
2020-08-19 20:23:46 -0300

    returning deepSearchPage import to original

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Route, HashRouter, Switch } from 'react-router-dom';
 import LandingPage from './components/LandingPage';
 import FoamTreeSearchPage from './components/quickSearchPage';
-import DeepSearchPage from './components/DeepSearchPage';
+import DeepSearchPage from './components/deepSearchPage';
 
 export default () => (
   <HashRouter>
