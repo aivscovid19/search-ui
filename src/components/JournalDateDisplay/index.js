16be051532929dd3b68b9d930ed143c209350b74
Victor <victor.as.nascimento@gmail.com>
2020-08-21 12:38:14 -0300

    verfying if date is available, if so rendering it and separator

--- src/components/JournalDateDisplay/index.js
+++ src/components/JournalDateDisplay/index.js
@@ -4,14 +4,19 @@ import './styles.css';
 // The keyword component takes a list as an argument
 
 export const JournalDateDisplay = ({ journal, date }) => {
+    journal = journal.length > 130
+    ? journal.slice(0, 130).trim() + "..."
+    : journal
     return (
         <div className="description-container">
             <span className="journal-styles">
                 {journal}
             </span>
-            <span className="date-styles">
-                02/04/2020
-            </span>
+            {date && (
+                <span className="date-styles">
+                    {date}
+                </span>
+            )}
         </div>
     );
 }
