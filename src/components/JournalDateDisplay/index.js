c4a61074c802c9f47ef08fdf0ad63be5f1517aca
Victor <victor.as.nascimento@gmail.com>
2020-08-21 13:19:19 -0300

    truncating if the string goes over the maximum width

--- src/components/JournalDateDisplay/index.js
+++ src/components/JournalDateDisplay/index.js
@@ -1,12 +1,7 @@
 import React from 'react';
 import './styles.css';
 
-// The keyword component takes a list as an argument
-
 export const JournalDateDisplay = ({ journal, date }) => {
-    journal = journal.length > 130
-    ? journal.slice(0, 130).trim() + "..."
-    : journal
     return (
         <div className="description-container">
             <span className="journal-styles">
