286c5206b6bf31a023aea6c26971d893fb2368af
Victor <victor.as.nascimento@gmail.com>
2020-08-26 21:36:16 -0300

    icon toggles full abstract

--- src/components/AbstractDisplay/index.js
+++ src/components/AbstractDisplay/index.js
@@ -1,15 +1,47 @@
-import React from 'react';
-import { Box, Typography } from '@material-ui/core';
+import React, { useState } from 'react';
+import ReactHtmlParser from 'react-html-parser'; 
+
+import VisibilityIcon from '@material-ui/icons/Visibility';
+
+import { Typography, IconButton } from '@material-ui/core';
+
+import * as icons from '../../icons';
 import './styles.css';
 
-export const AbstractDisplay = ({ abstract }) => {
-    console.log(abstract)
+export const AbstractDisplay = ({ abstract, highlight }) => {
+    const [isFullTextToggled, setFullTextToggled] = useState(false);
+    // const MAX_ABSTRACT = 250;
+
+    // const shortAbstract = (abstract.length >= MAX_ABSTRACT)
+    //   ? `${abstract.slice(0, MAX_ABSTRACT).trim()}...` : abstract;
+    const displayAbstract = highlight && highlight.abstract
+      ? ReactHtmlParser(highlight.abstract[0]) : abstract;
+
+    function handleToggleText(){
+        setFullTextToggled(isFullTextToggled === false ? true : false)
+    }
+
     return (
-        <Box className="abstract-container">
-            <Typography className="abstract-text" component="p" variant="subtitle1" color="textPrimary">
-                {abstract}
+        <>
+            <Typography
+             className={
+              isFullTextToggled
+              ? "abstract-text-toggled"
+              : "abstract-text"
+             }
+             component="p"
+             variant="subtitle1"
+             color="textPrimary"
+             >
+                {displayAbstract}
             </Typography>
-        </Box>
+            <IconButton
+                 onClick={() => { handleToggleText() }}
+                 size="small"
+            >
+                <VisibilityIcon />
+            </IconButton>
+        </>
     );
 }
 
